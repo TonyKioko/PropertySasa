f9a9a5668a7b06702038cee473f7d9bc2066e57d
TonyKioko <tonykioko384@gmail.com>
2018-10-24 22:55:35 +0300

    can loop thro' properties

--- listings/views.py
+++ listings/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-
+from listings.models import *
 # Create your views here.
 def index(request):
-
-    return render(request,'listings/listings.html')
+    listings = Listing.objects.all()
+    context= {"listings":listings}
+    return render(request,'listings/listings.html',context)
 
 def listing(request):
 
--- realtors/models.py
+++ realtors/models.py
@@ -7,7 +7,7 @@ class Realtor(models.Model):
     name = models.CharField(max_length=200)
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/',blank=True)
     description = models.TextField(max_length=400,blank=True)
-    phone = models.PositiveIntegerField(blank=True)
+    phone = models.PositiveIntegerField(blank=True,null=True)
     email = models.EmailField(max_length=40)
     is_mvp = models.BooleanField(default=False)
     hire_date = models.DateTimeField(default=datetime.now,blank=True)
