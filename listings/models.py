61a69bbc43bb4a348d05334efb916d3b9e2cb091
TonyKioko <tonykioko384@gmail.com>
2018-10-24 18:20:16 +0300

    Listing model complete

--- listings/models.py
+++ listings/models.py
@@ -1,3 +1,31 @@
 from django.db import models
-
+form datetime import datetime
+from realtors.models import Realtor
 # Create your models here.
+
+class Listing(models.Model):
+    realtor = models.ForeignKey(Realtor,on_delete=models.DO_NOTHING)
+    title = models.CharField(max_lenght=80)
+    city = models.CharField(max_lenght=80)
+    county = models.CharField(max_lenght=80)
+    address = models.CharField(max_lenght=80)
+    description = models.TextField(max_lenght=400,blank=True)
+    price = models.PositiveIntegerField()
+    bedrooms = models.PositiveIntegerField()
+    bathrooms = models.DecimalField(max_digits=2,decimal_places=1)
+    garage = models.PositiveIntegerField(default=0)
+    sqft = models.PositiveIntegerField()
+    bedrooms = models.PositiveIntegerField()
+    lot_size = models.DecimalField(max_digits=6,decimal_places=1)
+    photo_main = models.ImageField(upload_to='photos/%Y/%m/%d/',blank=True)
+    photo_1 = models.ImageField(upload_to='photos/%Y/%m/%d/',blank=True)
+    photo_2 = models.ImageField(upload_to='photos/%Y/%m/%d/',blank=True)
+    photo_3 = models.ImageField(upload_to='photos/%Y/%m/%d/',blank=True)
+    photo_4 = models.ImageField(upload_to='photos/%Y/%m/%d/',blank=True)
+    photo_5 = models.ImageField(upload_to='photos/%Y/%m/%d/',blank=True)
+    photo_6 = models.ImageField(upload_to='photos/%Y/%m/%d/',blank=True)
+    is_published = models.BooleanField(default=True
+    list_date = models.DateTimeField(default=datetime.now,blank=True)
+
+    def __str__(self):
+        return self.title
--- propertysasa/settings.py
+++ propertysasa/settings.py
@@ -65,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -118,6 +119,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
