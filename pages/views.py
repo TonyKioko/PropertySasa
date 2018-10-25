4f3a928a0db97c9447f6cc77af6167c9dfc2744d
TonyKioko <tonykioko384@gmail.com>
2018-10-25 11:10:39 +0300

    properties now displaying on homepage

--- pages/views.py
+++ pages/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404,HttpResponseRedirect
+from listings.models import Listing
+from realtors.models import Realtor
 
 # Create your views here.
 
 def index(request):
 
-    return render(request,'pages/index.html')
+    listings = Listing.objects.all()[:3]
+    context ={"listings":listings}
+    return render(request,'pages/index.html',context)
 
 def about(request):
 
