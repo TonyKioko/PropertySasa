a21581d6e05bf503bd7842c1b62e820ae8c520bf
TonyKioko <tonykioko384@gmail.com>
2018-10-25 11:57:30 +0300

    realtors images now dispalying

--- pages/views.py
+++ pages/views.py
@@ -14,6 +14,6 @@ def index(request):
 def about(request):
     realtors = Realtor.objects.order_by('-hire_date')
     mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
-    context = {"Realtor":realtors,"mvp_realtors":mvp_realtors}
+    context = {"realtors":realtors,"mvp_realtors":mvp_realtors}
 
     return render(request,'pages/about.html',context)
