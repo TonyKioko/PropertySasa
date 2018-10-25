f5bc8834de869f64386aa9f22b16bc390e51f922
TonyKioko <tonykioko384@gmail.com>
2018-10-25 22:41:54 +0300

    impleting search functionality in listing views.py

--- listings/views.py
+++ listings/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,redirect,get_object_or_404
 from listings.models import *
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from .choices import *
+
 # Create your views here.
 def index(request):
     listings = Listing.objects.order_by('-list_date')
@@ -12,8 +14,14 @@ def index(request):
 
 def listing(request,listing_id):
     listing = get_object_or_404(Listing,pk=listing_id)
-    context = {"listing":listing}
+    context = {"listing":listing,}
     return render(request,'listings/listing.html',context)
 def search(request):
+    queryset_list = Listing.objects.order_by('-list_date')
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            queryset_list=queryset_list.filter(description__icontains=keywords)
 
-    return render(request,'listings/search.html')
+    context = {"price_choices":price_choices,"bedroom_choices":bedroom_choices,"county_choices":county_choices,"listings":queryset_list}
+    return render(request,'listings/search.html',context)
