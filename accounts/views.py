5ab06915937ab597effb2a1c134490d6e15cb598
TonyKioko <tonykioko384@gmail.com>
2018-10-26 12:41:05 +0300

    user login working

--- accounts/views.py
+++ accounts/views.py
@@ -29,6 +29,19 @@ def register(request):
         return render(request,'accounts/register.html')
 def login(request):
         if request.method == 'POST':
+            username=request.POST['username']
+            password=request.POST['password']
+            user=auth.authenticate(username=username,password=password)
+
+            if user is not None:
+                auth.login(request,user)
+                messages.success(request,"You are now logged in")
+                return redirect('dashboard')
+            else:
+                messages.error(request,"Invalid Credentials")
+                return redirect('login')
+
+
             print('SUBMIT')
             # return redirect('register')
         else:
